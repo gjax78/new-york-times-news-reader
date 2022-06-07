d4500fb9818b8a9ec165163f8a3b01a98f877ce0
Geena Jackson <gjacks303@gmail.com>
2022-06-06 20:26:18 -0600

    Set up outline of functions

---
+++ src/components/ArticleCard/ArticleCard.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const ArticleCard = () => {
+  <h1>ArticleCard</h1>
+}
+
+export default ArticleCard
---
+++ src/components/DetailedView/DetailedView.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const DetailedView = () => {
+  <h1>DetailedView</h1>
+}
+
+export default DetailedView
