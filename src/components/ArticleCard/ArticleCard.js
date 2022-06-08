c7ca57d52094e584230000b9c017729c8b9bf020
Geena Jackson <gjacks303@gmail.com>
2022-06-07 21:40:57 -0600

    Remove photo:

--- src/components/ArticleCard/ArticleCard.js
+++ src/components/ArticleCard/ArticleCard.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const ArticleCard = ({ title, abstract, id, multimedia }) => {
   return(
     <article className='article-card'>
-      <img src={multimedia[2].url} alt={multimedia[2].caption} />
+      {/* <img src={multimedia[2].url} alt={multimedia[2].caption} /> */}
       <Link to={`article/${id}`}>
         <h2>{title}</h2>
       </Link>
