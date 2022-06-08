c5a9e6cd12307b6580df1ac73f8583bcb99595e2
Geena Jackson <gjacks303@gmail.com>
2022-06-07 21:26:22 -0600

    Add link to home button

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import './Header.css'
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
     <header className='header'>
       <h1>NY Times News Reader</h1>
-      <button className='home-button'>Home</button>
+      <Link to='/'>
+        <button className='home-button'>Home</button>
+      </Link>
     </header>
   )
 }
