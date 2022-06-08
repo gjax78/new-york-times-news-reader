956ccf2ffcf0b15144f2696316db710ce33c3ffc
Geena Jackson <gjacks303@gmail.com>
2022-06-07 18:21:51 -0600

    Set up articles component

--- src/components/Articles/Articles.js
+++ src/components/Articles/Articles.js
@@ -1,7 +1,28 @@
-import React from 'react'
+import './Articles.css'
+import ArticleCard from '../ArticleCard/ArticleCard'
 
-const Articles = () => {
-  <h1>Articles</h1>
+const Articles = ({ articles }) => {
+  let counter = 0
+
+  const cards = articles.map(article => (
+    <ArticleCard
+      key={counter++}
+      genre={article.section}
+      title={article.title}
+      abstract={article.abstract}
+      url={article.url}
+      byline={article.byline}
+      published={article.published_date}
+      image={article.multimedia[0].url}
+      alt={article.multimedia[0].caption}
+    />
+  ))
+
+  return (
+    <section className='articles-section'>
+      <div className='card-container'>{cards}</div>
+    </section>
+  )
 }
 
 export default Articles
