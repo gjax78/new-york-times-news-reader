a472b06b258af3c15678a592fb7b719b3dfd69b6
Geena Jackson <gjacks303@gmail.com>
2022-06-07 18:00:17 -0600

    Set up fetch call

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,21 @@
 import './App.css';
+import React, {useState, useEffect} from 'react'
+import fetchAPI from '../../apiCalls'
 
 const App = () => {
+  const [articles, setArticles] = useState([])
+  const [error, setError] = useState('')
+
+  const fetchStories = () => {
+    fetchAPI.getData()
+       .then((data) => setArticles(data.results))
+       .catch((error) => setError(error))  
+    }
+
+  useEffect(() => {
+    fetchStories()
+  }, [])
+
   return (
     <div className="App">
       <h1>NY Times News Reader</h1>
