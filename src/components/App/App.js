b3d2e9b644eb03831b0e3973e31694754dc19f36
Geena Jackson <gjacks303@gmail.com>
2022-06-07 18:21:36 -0600

    Set up router for articles

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,9 @@ import './App.css';
 import React, {useState, useEffect} from 'react'
 import fetchAPI from '../../apiCalls'
 import Header from '../Header/Header'
+import Articles from '../Articles/Articles'
+import { Route} from 'react-router-dom'
+
 
 const App = () => {
   const [articles, setArticles] = useState([])
@@ -20,6 +23,9 @@ const App = () => {
   return (
     <div className="App">
       <Header />
+      <Route exact path='/'>
+        <Articles articles={articles} />
+      </Route>
     </div>
   )
 }
