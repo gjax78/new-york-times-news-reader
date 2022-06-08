68df40a07a990fe6c7dc004acc5404d88a3e282a
Geena Jackson <gjacks303@gmail.com>
2022-06-07 21:18:17 -0600

    Refactor article card

--- src/components/Articles/Articles.js
+++ src/components/Articles/Articles.js
@@ -2,22 +2,21 @@ import './Articles.css'
 import ArticleCard from '../ArticleCard/ArticleCard'
 
 const Articles = ({ articles }) => {
-  const cards = articles.map(article => (
+  const cards = articles.map((article, index) => (
     <ArticleCard
-      key={Date.now()}
-      genre={article.section}
+      key={index}
+      id={article.short_url.split('/')[3]}
       title={article.title}
       abstract={article.abstract}
-      url={article.url}
-      byline={article.byline}
-      published={article.published_date}
       multimedia={article.multimedia}
-    />
+      />
   ))
 
   return (
     <section className='articles-section'>
-      <div className='card-container'>{cards}</div>
+      <div className='card-container'>
+        {cards}
+      </div>
     </section>
   )
 }
