18b183f4ef106001bda06591de4694407215223d
Geena Jackson <gjacks303@gmail.com>
2022-06-08 19:15:27 -0600

    Add features for detailed view of article

--- src/components/DetailedView/DetailedView.js
+++ src/components/DetailedView/DetailedView.js
@@ -7,8 +7,21 @@ const DetailedView = ({ articles, id }) => {
   return(
     <div className='details-viewpage'>
       <Link to='/'>
-        <button>Go Back</button>
+        <button>Back</button>
       </Link>
+      <div className='article-details'>
+        <div className='img-container'>
+          <img src={selectedArticle.multimedia[1].url} alt={selectedArticle.multimedia[1].caption} />
+        </div>
+        <div className='details-container'>
+          <h1>{selectedArticle.title}</h1>
+          <p>{selectedArticle.abstract}</p>
+          <p>{selectedArticle.byline}</p>
+          <a className='article-link' href={selectedArticle.short_url} target='_blank'>Read More</a>
+          <p>{selectedArticle.updated_date}</p>
+          <p>{selectedArticle.section} {selectedArticle.subsection}</p>
+        </div>
+      </div>
     </div>
   )
 }
