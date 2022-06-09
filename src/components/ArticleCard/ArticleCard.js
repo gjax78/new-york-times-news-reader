99f62365bd35a25bc41e255e5702858e00a04223
Geena Jackson <gjacks303@gmail.com>
2022-06-08 18:26:54 -0600

    Add conditional rendering for photo

--- src/components/ArticleCard/ArticleCard.js
+++ src/components/ArticleCard/ArticleCard.js
@@ -2,9 +2,15 @@ import './ArticleCard.css'
 import { Link } from 'react-router-dom';
 
 const ArticleCard = ({ title, abstract, id, multimedia }) => {
+  let image;  
+  
+  if (multimedia) {
+    image = <img src={multimedia[1].url} alt={multimedia[1].caption} />
+  }
+
   return(
     <article className='article-card'>
-      <img src={multimedia[1].url} alt={multimedia[1].caption} />
+      {image}
       <Link to={`article/${id}`}>
         <h2>{title}</h2>
       </Link>
